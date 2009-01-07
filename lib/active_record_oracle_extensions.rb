3f4d5eca4049f97c38ae319c2501aa0232b7662c
Rob Christie <robchristie@gmail.com>
2009-01-07 03:04:12 -0500

    Added support for the OracleEnhancedAdapter.

--- lib/active_record_oracle_extensions.rb
+++ lib/active_record_oracle_extensions.rb
@@ -2,6 +2,14 @@
 ActiveRecord::SchemaDumper.send(:include, Extensions::ActiveRecord::SchemaDumper)
 ActiveRecord::ConnectionAdapters::AbstractAdapter.send(:include, Extensions::ActiveRecord::ConnectionAdapters::AbstractAdapter)
 ActiveRecord::ConnectionAdapters::SchemaStatements.send(:include, Extensions::ActiveRecord::ConnectionAdapters::SchemaStatements)
-ActiveRecord::ConnectionAdapters::OracleAdapter.send(:include, Extensions::ActiveRecord::ConnectionAdapters::OracleAdapter)
+
+if defined?(ActiveRecord::ConnectionAdapters::OracleAdapter) then
+  ActiveRecord::ConnectionAdapters::OracleAdapter.send(:include, Extensions::ActiveRecord::ConnectionAdapters::OracleAdapter)
+end
+
+if defined?(ActiveRecord::ConnectionAdapters::OracleEnhancedAdapter) then
+  ActiveRecord::ConnectionAdapters::OracleEnhancedAdapter.send(:include, Extensions::ActiveRecord::ConnectionAdapters::OracleAdapter)
+end
+
 # patching habtm preload fetch that tries to use add AS before a table alias
 ActiveRecord::Base.send(:include, Extensions::ActiveRecord::AssociationPreload)
